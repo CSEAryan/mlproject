4c370e5ca09757ed73f5344a7d5e1be452718c2a
CSEAryan <dipuaryan1111@gmail.com>
2024-12-10 17:28:52 +0545

    updated final

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import r2_score
 
@@ -19,7 +20,7 @@ def save_object(file_path, obj):
 
         with open(file_path,'wb') as file_obj:
             #used to store the pickel file in harddisk
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
     
@@ -47,12 +48,21 @@ def evaluate_model(X_train, y_train,X_test, y_test, models, param):
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-            
+
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
